7e96d0f8deb39c60d82b03059886fdf4941d72c9
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 00:33:24 +0700

    feat(auth): implement login function in Auth controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { RegisterUser } from './dto/RegisterUser.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   async register(@Body() body: RegisterUser) {
     return this.authService.register(body);
+  }
+
+  @Post('login')
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
