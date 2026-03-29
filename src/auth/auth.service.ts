f12a89b995662aeafd11e66091feeb49155ddc18
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 00:33:38 +0700

    feat(auth): implement login function in Auth service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,24 @@
-import { Injectable, Inject, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  Inject,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DRIZZLE } from '../database/database.module';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as schema from '../database/schema';
 import * as bcrypt from 'bcrypt';
 import { eq } from 'drizzle-orm';
 import { RegisterUser } from './dto/RegisterUser.dto';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(@Inject(DRIZZLE) private db: PostgresJsDatabase<typeof schema>) {}
+  constructor(
+    @Inject(DRIZZLE) private db: PostgresJsDatabase<typeof schema>,
+    private jwtService: JwtService,
+  ) {}
 
   async register(userData: RegisterUser) {
     // 1. Cek apakah user sudah ada
@@ -32,5 +42,41 @@ export class AuthService {
         password: hashedPassword,
       })
       .returning();
+  }
+
+  async login(loginDto: LoginDto) {
+    const [user] = await this.db
+      .select()
+      .from(schema.users)
+      .where(eq(schema.users.npm_atau_nip, loginDto.npm_atau_nip))
+      .limit(1);
+
+    if (!user) {
+      throw new UnauthorizedException('NPM/NIP tidak ditemukan');
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Password salah');
+    }
+
+    const payload = {
+      sub: user.id,
+      npm: user.npm_atau_nip,
+      role: user.role,
+      nama: user.nama,
+    };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+      user: {
+        nama: user.nama,
+        role: user.role,
+      },
+    };
   }
 }
