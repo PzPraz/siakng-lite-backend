be78479ce9912552ab1baa592b2731e4bea665c9
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 00:31:43 +0700

    feat(auth): configurate JWT and add Auth controller as entry point in Auth module.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
+// src/auth/auth.module.ts
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    JwtModule.register({
+      global: true,
+      secret: process.env.JWT_SECRET || 'SECRET_COMPFEST_PZPRAZ',
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
 })
 export class AuthModule {}
