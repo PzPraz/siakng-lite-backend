7b0fe77e05b6908bf69f3c899a584a9f29038279
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 11:07:41 +0700

    feat(course): implement findAll, create, delete function in Course service.

--- src/course/course.service.ts
+++ src/course/course.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { DRIZZLE } from 'src/database/database.module';
+import * as schema from '../database/schema';
+import { CreateCourseDto } from './dto/create-course.dto';
+import { eq } from 'drizzle-orm';
 
 @Injectable()
-export class CourseService {}
+export class CourseService {
+  constructor(@Inject(DRIZZLE) private db: PostgresJsDatabase<typeof schema>) {}
+
+  async findAll() {
+    return await this.db.select().from(schema.courses);
+  }
+
+  async create(data: CreateCourseDto) {
+    return await this.db.insert(schema.courses).values(data).returning();
+  }
+
+  async delete(id: number) {
+    return await this.db
+      .delete(schema.courses)
+      .where(eq(schema.courses.id, id));
+  }
+}
