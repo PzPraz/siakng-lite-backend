97fb5fb9ece96315f4a66cd251ae65aa1c5c411e
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 18:19:27 +0700

    feat(classes): implement CRUD function for classes in classes Service

--- src/classes/classes.service.ts
+++ src/classes/classes.service.ts
@@ -3,6 +3,7 @@ import { DRIZZLE } from 'src/database/database.module';
 import * as schema from '../database/schema';
 import { eq, sql } from 'drizzle-orm';
 import { PostgresJsDatabase } from 'drizzle-orm/postgres-js';
+import { CreateClassDto } from './dto/create-class.dto';
 
 @Injectable()
 export class ClassesService {
@@ -24,5 +25,23 @@ export class ClassesService {
         schema.courses,
         eq(schema.classes.courseId, schema.courses.id),
       );
+  }
+
+  async create(dto: CreateClassDto) {
+    return await this.db.insert(schema.classes).values(dto).returning();
+  }
+
+  async update(id: number, dto: Partial<CreateClassDto>) {
+    return await this.db
+      .update(schema.classes)
+      .set(dto)
+      .where(eq(schema.classes.id, id))
+      .returning();
+  }
+
+  async delete(id: number) {
+    return await this.db
+      .delete(schema.classes)
+      .where(eq(schema.classes.id, id));
   }
 }
