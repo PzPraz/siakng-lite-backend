471e41edcc7eea765b740695b404865d05b18ba6
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-03-30 16:33:16 +0700

    feat(db): add classes, irs table to db

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,11 @@
-import { pgTable, serial, varchar, pgEnum } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  varchar,
+  pgEnum,
+  integer,
+  timestamp,
+} from 'drizzle-orm/pg-core';
 
 // Deklarasi Enum
 export const roleEnum = pgEnum('role', ['DOSEN', 'MAHASISWA']);
@@ -16,4 +23,27 @@ export const courses = pgTable('courses', {
   kode: varchar('kode', { length: 20 }).notNull().unique(),
   nama: varchar('nama', { length: 255 }).notNull(),
   sks: serial('sks').notNull(),
+});
+
+export const classes = pgTable('classes', {
+  id: serial('id').primaryKey(),
+  courseId: integer('course_id').references(() => courses.id, {
+    onDelete: 'cascade',
+  }),
+  namaKelas: varchar('nama_kelas', { length: 20 }).notNull(),
+  dosenId: integer('dosen_id').references(() => users.id),
+  kapasitas: integer('kapasitas').notNull().default(40),
+  jadwal: varchar('jadwal', { length: 100 }),
+});
+
+export const irs = pgTable('irs', {
+  id: serial('id').primaryKey(),
+  studentId: integer('student_id')
+    .references(() => users.id, { onDelete: 'cascade' })
+    .notNull(),
+  classId: integer('class_id')
+    .references(() => courses.id, { onDelete: 'cascade' })
+    .notNull(),
+  status: varchar('status', { length: 20 }).default('PENDING'),
+  createdAt: timestamp('created_at').defaultNow(),
 });
