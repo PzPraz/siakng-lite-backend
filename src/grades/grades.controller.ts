a243ddc13072d4fc1b469ebcbf76c4046a79b301
PzPraz <prasetyas7306@gmail.com>
2026-04-03 01:47:24 +0700

    feat(grades): add deleteComponent, updatePublishStatus, getStudentsGradeByClass, and gradeStudent function in Grades controller

--- src/grades/grades.controller.ts
+++ src/grades/grades.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Patch, UseGuards, Post, Param, Get } from '@nestjs/common';
+import { Body, Controller, Patch, UseGuards, Post, Param, Get, Request, ParseIntPipe, Delete } from '@nestjs/common';
 import { Roles } from 'src/auth/decorators/role.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { CreateGradeComponentDto } from './dto/create-grade-component.dto';
 import { GradesService } from './grades.service';
-
+import type { AuthenticatedRequest } from 'src/irs/irs.controller';
+import { CreateGradeDto } from './dto/create-grade.dto';
 
 @Controller('grades')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -27,5 +28,38 @@ export class GradesController {
   @Roles('DOSEN')
   async editComponent(@Body() payload: CreateGradeComponentDto, @Param('id') id: string) {
     return this.gradesService.editComponent(Number(id), payload);
+  }
+
+  @Delete(':id')
+  @Roles('DOSEN')
+  async deleteComponent(@Param('id') id: string) {
+    return this.gradesService.deleteComponent(Number(id));
+  }
+
+  @Patch(':classId/publish-status')
+  @Roles('DOSEN')
+  async updatePublishStatus(@Param('classId') classId: string, @Body() body: { isPublished: boolean}) {
+    return this.gradesService.setPublishStatus(Number(classId), body.isPublished);
+  }
+
+  @Get(':classId/students/:studentId/grades')
+  async getStudentGradeByClass(
+    @Param('classId', ParseIntPipe) classId: number,
+    @Param('studentId', ParseIntPipe) studentId: number,
+    @Request() req: AuthenticatedRequest,
+  ) {
+    const currentUser = req.user; 
+
+    return this.gradesService.getStudentGradeByClass(
+      studentId, 
+      classId, 
+      currentUser
+    );
+  }
+
+  @Post(':studentId')
+  @Roles('DOSEN')
+  async gradeStudent(@Body() body: CreateGradeDto[], @Param('studentId') id: string) {
+    return this.gradesService.gradeStudent(Number(id), body);
   }
 }
